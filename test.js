65f2bcb137be4c92031a06d614b9ae7e2b1c20ad
weixianfa <372301467@qq.com>
2019-11-28 16:09:31 +0800

    测试用例

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
 const check = require('./index');
 
-console.log('haha');
+let data = {
+  name: '韦现法',
+  tel: 123456,
+  year: 12
+}
+
+let rule = {
+  name: '名字不可为空',
+  school: '学校不可为空',
+  tel: {
+    allowNull: true,
+    reg: /[0-9]/,
+    regErrmsg: '电话格式不正确'
+  },
+  year: {
+    nullmsg: '年龄字段不可为空',
+    reg: /s/,
+    regErrmsg: '年龄格式不正确'
+  }
+}
+
+let errmsg = check(data, rule);
+console.log(errmsg);
