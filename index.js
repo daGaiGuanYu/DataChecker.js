3cb74422072750717f1eceed97065b62fde7e968
weixianfa <372301467@qq.com>
2019-11-28 15:56:07 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = function check(data, rule){
+  if(!data||!rule)
+    throw Error('数据对象或规则不可为空');
+  
+  
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+const check = require('./index');
+
+console.log('haha');
